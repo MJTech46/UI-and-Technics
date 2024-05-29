ab78903d367f6f3d69f356522e4ba25210a1eaef
MJTech46 <140804026+MJTech46@users.noreply.github.com>
2024-05-29 20:42:25 +0530

    added account page

--- BS5.3-Poll-App/src/main.js
+++ BS5.3-Poll-App/src/main.js
@@ -1,11 +1,22 @@
 // Toggling the theme
 const modeButton = document.getElementById("modeButton");
 const htmlElement = document.querySelector("html");
+const themeColor = localStorage.getItem("themeColor");
+
+// To loading the data from the localstorage
+if(themeColor === "dark"){
+    htmlElement.dataset.bsTheme = "dark";
+} else {
+    htmlElement.dataset.bsTheme = "light";
+}
+
 modeButton.addEventListener("click", () => {
     if (htmlElement.dataset.bsTheme === "light") {
         htmlElement.dataset.bsTheme = "dark";
+        localStorage.setItem("themeColor", "dark");
     } else {
         htmlElement.dataset.bsTheme = "light";
+        localStorage.setItem("themeColor", "light")
     }
 });
 
