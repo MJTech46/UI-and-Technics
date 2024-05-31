d306ae7698da33ba92a6e2000d4fc5b5392f05ae
MJTech46 <140804026+MJTech46@users.noreply.github.com>
2024-05-31 17:34:27 +0530

    create poll page added

--- BS5.3-Poll-App/src/main.js
+++ BS5.3-Poll-App/src/main.js
@@ -24,4 +24,49 @@ modeButton.addEventListener("click", () => {
 const navElement = document.querySelector("nav"); 
 const mainBodyElement = document.getElementById("mainBody");
 const navHeight = navElement.offsetHeight;
-mainBodyElement.style.paddingTop = `${navHeight + 15}px`;
+//mainBodyElement.style.paddingTop = `${navHeight + 15}px`;
+
+// for createPoll page
+const addOptionBtn = document.getElementById("addOption");
+const deleteOptionBtn = document.getElementById("deleteOption");
+const optionsDiv = document.getElementById("options");
+const optionsCountInput = document.getElementById("optionsCount");
+var optionCounter = 2;
+optionsCountInput.value = optionCounter;
+console.log(optionsCountInput.value)
+
+addOptionBtn.addEventListener("click", () => {
+    // Create a new input element
+    const newInput = document.createElement("input");
+    newInput.id = `answerOptions${optionCounter + 1}`; // Increment the ID for each new option
+    newInput.type = "text";
+    newInput.className = "form-control mb-3";
+    newInput.placeholder = `Option ${optionCounter + 1}`;
+    newInput.required = true;
+    // Append the new input element to the optionsDiv
+    optionsDiv.appendChild(newInput);
+    // Increment the optionCounter
+    optionCounter++;
+    optionsCountInput.value = optionCounter;
+    console.log("Added:",optionsCountInput.value)
+
+    if (optionCounter > 2) {
+        deleteOptionBtn.hidden = false;
+    } else {
+        deleteOptionBtn.hidden = true;
+    }
+});
+
+deleteOptionBtn.addEventListener("click", () =>{
+    const lastInput = document.getElementById(`answerOptions${optionCounter}`);
+    if (optionCounter > 2) {
+        lastInput.remove();
+        optionCounter--;
+        optionsCountInput.value = optionCounter;
+        console.log("Removed:", optionsCountInput.value)
+    }
+    if (optionCounter === 2) {
+        deleteOptionBtn.hidden = true;
+    }
+     
+});
