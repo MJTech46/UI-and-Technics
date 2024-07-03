cb86b74ba652d56bbcd4020ef626b67e15101312
MJTech46 <140804026+MJTech46@users.noreply.github.com>
2024-07-03 12:34:23 +0530

    added progressColor for Quiz

--- BS5.3-quiz-app/main.js
+++ BS5.3-quiz-app/main.js
@@ -3,9 +3,27 @@ const avilableTime = 10; // in seconds
 const refreshRate = 16; // in Hertz(Hz)
 
 var currentTime = 0;
+var red = 0;
+var green = 255;
+
 const updater = setInterval(() => {
     //console.log(currentTime, (100 - (currentTime*(100/avilableTime))) + "%");
     progressBar.style.width = (100 - (currentTime*(100/avilableTime))) + "%";
+
+    // green to red
+    progressBar.style.backgroundColor = "rgb(" + (currentTime*(255/avilableTime)) + "," + (255 - (currentTime*(255/avilableTime))) +", 0)";
+    
+    // green to yellow to red
+    /*if(red < green){
+        red=(currentTime*2*(255/avilableTime));
+    }else if(red > green){
+        green=((avilableTime-currentTime)*(255/avilableTime));
+    }
+    else{
+        green--; // red === green
+    }
+    progressBar.style.backgroundColor = "rgb("+red+","+green+", 0)";*/
+
     if (currentTime >= avilableTime){
         clearInterval(updater);
     }
